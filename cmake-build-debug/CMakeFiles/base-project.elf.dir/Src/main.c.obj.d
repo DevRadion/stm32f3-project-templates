8c064d1d5fd4a5d475222578883431225dd7a860
Radion Rusnak <radion.rusnak@gmail.com>
2024-04-25 18:38:42 +0300

    Added .gitignore

--- cmake-build-debug/CMakeFiles/base-project.elf.dir/Src/main.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/base-project.elf.dir/Src/main.c.obj: \
- /home/radion/CLionProjects/stm32/base-project/Src/main.c \
- /home/radion/CLionProjects/stm32/base-project/Inc/main.h
--- cmake-build-debug/CMakeFiles/base-project.elf.dir/Src/system_config.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/base-project.elf.dir/Src/system_config.c.obj: \
- /home/radion/CLionProjects/stm32/base-project/Src/system_config.c \
- /home/radion/CLionProjects/stm32/base-project/Inc/system_config.h
