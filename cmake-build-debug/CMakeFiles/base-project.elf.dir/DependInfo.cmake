7ff99d1b761ba708e227b9ba8cc82ddbf883c97e
Radion Rusnak <radion.rusnak@gmail.com>
2024-04-25 18:08:20 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/base-project.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/radion/CLionProjects/stm32/base-project/Startup/startup_stm32f303retx.s" "/home/radion/CLionProjects/stm32/base-project/cmake-build-debug/CMakeFiles/base-project.elf.dir/Startup/startup_stm32f303retx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F303xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/radion/CLionProjects/stm32/base-project/Inc"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Inc"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/main.c" "CMakeFiles/base-project.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/main.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/stm32f3xx_hal_msp.c" "CMakeFiles/base-project.elf.dir/Src/stm32f3xx_hal_msp.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/stm32f3xx_hal_msp.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/stm32f3xx_it.c" "CMakeFiles/base-project.elf.dir/Src/stm32f3xx_it.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/stm32f3xx_it.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/syscalls.c" "CMakeFiles/base-project.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/syscalls.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/sysmem.c" "CMakeFiles/base-project.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/sysmem.c.obj.d"
+  "/home/radion/CLionProjects/stm32/base-project/Src/system_stm32f3xx.c" "CMakeFiles/base-project.elf.dir/Src/system_stm32f3xx.c.obj" "gcc" "CMakeFiles/base-project.elf.dir/Src/system_stm32f3xx.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/base-project.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for base-project.elf.
+# This may be replaced when dependencies are built.
